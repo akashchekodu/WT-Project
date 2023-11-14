cbaf8e2c2fa8bbd4e5b73820b652392851b39d6a
Akash C <chekodu.akash30@gmail.com>
2023-11-14 08:33:11 +0530

    update

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -10,6 +10,7 @@ class APIFeatures {
     excludedFields.forEach((el) => delete queryObj[el]);
 
     // 1B) Advanced filtering
+
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
